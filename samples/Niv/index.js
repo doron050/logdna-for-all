083dfc3aa734532aba97f311dc6570efc50b3514
NM <50276767+NivM1@users.noreply.github.com>
2019-05-29 00:31:44 +0300

    add example of getting basic deploy info and the last deploy logs

---
+++ samples/Niv/index.js
@@ -0,0 +1,47 @@
+const {
+    withUiHook,
+    htm
+} = require('@zeit/integration-utils')
+
+let count = 0
+
+module.exports = withUiHook(async ({
+    payload, zeitClient 
+}) => {
+    console.log(payload);
+    const u = payload.user;
+    console.log(u);
+    const p = payload.project;
+    console.log(p);
+    const t = payload.team;
+    console.log(t);
+
+    const {projectId} = payload;
+	let apiUrl = `/v4/now/deployments?limit=10`;
+	if (projectId) {
+		apiUrl += `&projectId=${projectId}`
+	}
+
+    const {deployments} = await zeitClient.fetchAndThrow(apiUrl, {method: 'GET'});
+    
+    console.log(deployments);
+
+    const logs = await zeitClient.fetchAndThrow("/v2/now/deployments/" + "dpl_8qZ3rjJtDAK7E5DUoJN9ybz5rEem" + "/events", {method: 'GET'});
+    
+    console.log(logs.length);
+
+    const {
+        clientState,
+        action
+    } = payload;
+    count += 1
+    return htm`
+    <Page>
+    <P>project: ${p}</P>
+    <P>client: ${u}</P>
+    <P>client: ${t}</P>
+      <P>Counter: ${count}</P>
+      <Button>Count Me</Button>
+    </Page>
+  `
+})
---
+++ samples/Niv/node_modules/htm/preact/index.mjs
@@ -0,0 +1,1 @@
+import{h as r,Component as t,render as o}from"preact";export{h,Component}from"preact";import e from"htm";function m(r,t){o(r,t,t.firstElementChild)}var n=e.bind(r);export{n as html,m as render};
