a345efa9b87854461f4af7c06d17a05728df77cb
Moshe <m.bmw535i@gmail.com>
2019-05-31 15:44:36 +0300

    refactored winston

--- app.js
+++ app.js
@@ -1,38 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const constants = require('./common/constants');
-const _logdnaWinstonTransport = require('logdna-winston');
-// const _LogzioWinstonTransport = require('winston-logzio');
-const winston = require('winston');
+const logger = require('./logSender/winston');
 
 dotenv.config();
 const app = express();
-const logger = winston.createLogger({});
-const logdnaWinstonTransport = new _logdnaWinstonTransport({
-    key: constants.LOGSDNA_KEY
-});
-// const logzioWinstonTransport = new _LogzioWinstonTransport({
-//     // level: 'info',
-//     name: 'winston_logzio',
-//     token: constants.LOGZIO_TOKEN
-// });
-
-
-
-logger.add(logdnaWinstonTransport);
-// logger.add(logzioWinstonTransport);
-
 
 
 app.get('/hi', async (req, res) => {
 
-    logger.log("info", "logger.log this data");
-
-    logger.log({
-        level: "debug",
-        message: "data2"
-    });
-
+    logger.sendLog('debug',"This is a test log by Moshe");
     res.send('hello world');
 });
 
