c2a49cd31140242dac72b2025b588f68e634a159
Moshe <m.bmw535i@gmail.com>
2019-06-01 01:46:23 +0300

    change from configID to projectID

--- common/constants.js
+++ common/constants.js
@@ -20,11 +20,11 @@ const LOG_MESSAGES = {
     ERROR_GET_LOGS: "Error get logs: ",
     SUCCESS_GET_ACCESS_TOKEN: "Access token retrive: ",
     ERROR_GET_ACCESS_TOKEN: "Error! can't retrive access token: ",
-    NEW_LOGGER: "Creating new logger for client with configuration ID: ",
-    NEW_CLIENT: "Found new client with configuration ID: ",
-    HANDLING_CLIENT: "Currently handling client with configuration ID: ",
-    STATUS_CHANGE: "Change of status for Client with configuration ID: ",
-    TERMINATION_NOTICE: "killing cycle for client with configuration ID: "
+    NEW_LOGGER: "Creating new logger for client with project ID: ",
+    NEW_CLIENT: "Found new client with project ID: ",
+    HANDLING_CLIENT: "Currently handling client with project ID: ",
+    STATUS_CHANGE: "Change of status for Client with project ID: ",
+    TERMINATION_NOTICE: "killing cycle for client with project ID: "
 
 };
 
--- common/zeitTOdna.js
+++ common/zeitTOdna.js
@@ -2,7 +2,7 @@ function convertToDNA(zeitLog,clientToHandle) {
     const dnaLog = {
         deploymentId: zeitLog.payload.deploymentId,
         data: zeitLog.payload.text,
-        id: clientToHandle.configurationId
+        id: clientToHandle.projects.projectId
     }
 
     return dnaLog;
