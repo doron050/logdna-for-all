c71dc23b7e8aef502cb7f71b6476b096c4d47990
Doron Eli <doron.eli@naturalint.com>
2019-05-31 17:40:00 +0300

    try

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-const constants = require('./common/constants');
-const logger = require('./common/logger');
+const constants = require('common/constants');
+const logger = require('common/logger');
 
 const app = express();
 
