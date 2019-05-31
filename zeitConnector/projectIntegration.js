9ae9b456decc8f957cb269ff9a22818a79544d90
Niv M <nmahnovsky@gmail.com>
2019-05-31 11:11:29 +0300

    fix func

--- zeitConnector/projectIntegration.js
+++ zeitConnector/projectIntegration.js
@@ -1,42 +1,35 @@
 const constants = require('../common/constants');
 
-function getLogs(deploymentId) {
+async function getLogs(deploymentId) {
 
     // Make a request for a user with a given ID
-    axios.get(constants.ZEIT_API_ROUTES.DEPLOYMENTS + deploymentId + "/" + constants.ZEIT_API_ROUTES.LOGS)
-        .then(function (response) {
-            
-            console.log(constants.LOG_MESSAGES.SUCCESS_GET_LOGS);
+    try {
+        const response = await axios.get(constants.ZEIT_API_ROUTES.DEPLOYMENTS + deploymentId + "/" + constants.ZEIT_API_ROUTES.LOGS)
+        console.log(constants.LOG_MESSAGES.SUCCESS_GET_LOGS);
+        return (response.data)
+    } catch {
 
-            return (response.data)
-        })
-        .catch(function (error) {
-            
-            console.log(constants.LOG_MESSAGES.ERROR_GET_LOGS + error);
-        });
-};
+        console.log(constants.LOG_MESSAGES.ERROR_GET_LOGS + error);
+    };
+}
 
-function getDeployments(projectId, limit) {
+async function getDeployments(projectId, limit) {
 
     // If no limit passed, get the last deployment
     if (!limit) limit = 1;
 
-    axios.get(constants.ZEIT_API_ROUTES.DEPLOYMENTS, {
+    try {
+        const response = await axios.get(constants.ZEIT_API_ROUTES.DEPLOYMENTS, {
             params: {
                 limit: limit,
                 projectId: projectId
             }
-        })
-        .then(function (response) {
-            
-            console.log(constants.LOG_MESSAGES.SUCCESS_GET_INTEGRATIONS + response.length);
-
-            return (response.data);
-        })
-        .catch(function (error) {
-            
-            console.log(constants.LOG_MESSAGES.ERROR_GET_INTEGRATIONS + error);
-        })
+        });
+        console.log(constants.LOG_MESSAGES.SUCCESS_GET_INTEGRATIONS + response.length);
+        return (response.data);
+    } catch {
+        console.log(constants.LOG_MESSAGES.ERROR_GET_INTEGRATIONS + error);
+    }
 }
 
 module.exports = {
