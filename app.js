bdeb3a36a278f119974f31300b51fc115a495c9a
Moshe <m.bmw535i@gmail.com>
2019-05-31 22:52:15 +0300

    clientRetriver

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const constants = require('./common/constants');
 const dnaLogger = require('./common/dnaLogger');
-
+const clientRetriver = require('./clientRetriver');
 const app = express();
+const _clientRetriverPid = setInterval(() => clientRetriver.syncCollection(), 5000);
+
 
 app.get('/hi', async (req, res) => {
 
@@ -16,6 +18,10 @@ app.get('/hi', async (req, res) => {
 
 app.get('/keepalive', async (req, res) => {
     res.send("thank you");
+});
+
+app.get('/sync',async (req,res) => {
+    
 });
 
 app.listen(constants.PORT, () => console.log(`app on port ${constants.PORT}`));
