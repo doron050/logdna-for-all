3d28262e3f70d4bf81734c37ff1fab299233a162
Niv M <nmahnovsky@gmail.com>
2019-05-31 11:04:40 +0300

    logs and deployments api

--- common/constants.js
+++ common/constants.js
@@ -1,18 +1,32 @@
-const API_BASIC_ENDPOINT_URL = "https://api.zeit.co";
+const API_BASIC_ENDPOINT_URL = "https://api.zeit.co/v2/now/";
 const LOGZIO_TOKEN = "037b265e-c010-49f6-9a99-ecfe29d5adb8";
 const MONGO_URL = "mongodb://localhost:27017/";
 const MONGO_DB = "mydb";
 
+const ZEIT_API_ROUTES = {
 
+    LOGS: "/events",
+    DEPLOYMENTS: "/deployments"
+};
 
 const LOG_MESSAGES = {
 
-    NEW_INTEGRATION: "New integration init: "
+    NEW_INTEGRATION: "New integration init: ",
+    SUCCESS_GET_INTEGRATIONS: "Success get last integrations: ",
+    ERROR_GET_INTEGRATIONS: "Error get last integrations: ",
+    SUCCESS_GET_LOGS: "Success get logs: ",
+    ERROR_GET_LOGS: "Error get logs: "
 };
+
+const ZEIT_HTTP_INSTANCE = axios.create({
+    baseURL: API_BASIC_ENDPOINT_URL
+});
 
 module.exports = {
     API_BASIC_ENDPOINT_URL,
     LOG_MESSAGES,
     LOGZIO_TOKEN,
-    MONGO_URL
+    MONGO_URL,
+    ZEIT_API_ROUTES,
+    ZEIT_HTTP_INSTANCE
 };
--- zeitConnector/projectIntegration.js
+++ zeitConnector/projectIntegration.js
@@ -1,2 +1,45 @@
 const constants = require('../common/constants');
 
+function getLogs(deploymentId) {
+
+    // Make a request for a user with a given ID
+    axios.get(constants.ZEIT_API_ROUTES.DEPLOYMENTS + deploymentId + "/" + constants.ZEIT_API_ROUTES.LOGS)
+        .then(function (response) {
+            
+            console.log(constants.LOG_MESSAGES.SUCCESS_GET_LOGS);
+
+            return (response.data)
+        })
+        .catch(function (error) {
+            
+            console.log(constants.LOG_MESSAGES.ERROR_GET_LOGS + error);
+        });
+};
+
+function getDeployments(projectId, limit) {
+
+    // If no limit passed, get the last deployment
+    if (!limit) limit = 1;
+
+    axios.get(constants.ZEIT_API_ROUTES.DEPLOYMENTS, {
+            params: {
+                limit: limit,
+                projectId: projectId
+            }
+        })
+        .then(function (response) {
+            
+            console.log(constants.LOG_MESSAGES.SUCCESS_GET_INTEGRATIONS + response.length);
+
+            return (response.data);
+        })
+        .catch(function (error) {
+            
+            console.log(constants.LOG_MESSAGES.ERROR_GET_INTEGRATIONS + error);
+        })
+}
+
+module.exports = {
+    getLogs,
+    getDeployments
+};
