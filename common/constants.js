633518124f72e3bbddfec65fa027b55527081f95
Niv M <nmahnovsky@gmail.com>
2019-05-31 10:18:41 +0300

    export

--- common/constants.js
+++ common/constants.js
@@ -1,2 +1,12 @@
 
 const API_BASIC_ENDPOINT_URL = "https://api.zeit.co";
+
+const LOG_MESSAGES = {
+    
+    NEW_INTEGRATION: "New integration init: "
+};
+
+module.exports = {
+    API_BASIC_ENDPOINT_URL,
+    LOG_MESSAGES
+};
