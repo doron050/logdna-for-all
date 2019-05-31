2022d944c8c57b2391f739fc771b253253bb4483
Doron Eli <doron.eli@naturalint.com>
2019-05-31 13:25:10 +0300

    add app.js

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-// ** StoryTeller Server **
-// 3rd Server dependencies
 const express = require('express');
 const dotenv = require('dotenv');
-dotenv.config();
-
 const constants = require('./common/constants');
-
+dotenv.config();
 const app = express();
-
 
 app.get('/hi', async (req, res) => {
     console.log('hi');
     res.send('hello world');
 });
 
-// Set the server to listen to requests
-app.listen(constants.PORT, () => console.log(`app on`));
+
+app.listen(constants.PORT, () => console.log(`app on port ${constants.PORT}`));
