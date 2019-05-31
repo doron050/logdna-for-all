d0f0cf6f02f11dd053f7ee587d52ddae2665791c
Niv M <nmahnovsky@gmail.com>
2019-05-31 23:54:56 +0300

    http client

--- common/httpClient.js
+++ common/httpClient.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const constants = require('./constants');
 
 const ZEIT_HTTP_INSTANCE = axios.create({
-    baseURL: API_BASIC_ENDPOINT_URL
+    baseURL: constants.API_BASIC_ENDPOINT_URL
 });
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
