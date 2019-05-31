48fca1b5499e2ac0a7fafc280648aebf24bd4240
Niv M <nmahnovsky@gmail.com>
2019-05-31 10:24:52 +0300

    a

--- zeitConnector/zeitClient.js
+++ zeitConnector/zeitClient.js
@@ -1,11 +1,21 @@
-const constants = require('./common/constants');
+const constants = require('../common/constants');
 
 module.exports = withUiHook(async ({
     payload, zeitClient 
 }) => {
     
-    console.log("Start");
+    console.log(constants.LOG_MESSAGES.NEW_INTEGRATION);
 
+    const integrationDetails = {
+        
+    }
+    console.log(payload);
+    const u = payload.user;
+    console.log(u);
+    const p = payload.project;
+    console.log(p);
+    const t = payload.team;
+    console.log(t);
     const {projectId} = payload;
 	let apiUrl = `/v4/now/deployments?limit=10`;
 	if (projectId) {
