c3cdaa4eeda57d429705ab0d23d204a273fceb3e
Moshe <m.bmw535i@gmail.com>
2019-05-31 15:31:09 +0300

    logdna works

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const constants = require('./common/constants');
+const _logdnaWinstonTransport = require('logdna-winston');
+const _LogzioWinstonTransport = require('winston-logzio');
+const winston = require('winston');
+
 dotenv.config();
 const app = express();
+const logger = winston.createLogger({});
+const logdnaWinstonTransport = new _logdnaWinstonTransport({
+    key: constants.LOGSDNA_KEY
+});
+// const logzioWinstonTransport = new _LogzioWinstonTransport({
+//     // level: 'info',
+//     name: 'winston_logzio',
+//     token: constants.LOGZIO_TOKEN
+// });
+
+
+
+logger.add(logdnaWinstonTransport);
+// logger.add(logzioWinstonTransport);
+
+
 
 app.get('/hi', async (req, res) => {
-    console.log('hi');
+
+    logger.log("info", "logger.log this data");
+
+    logger.log({
+        level: "debug",
+        message: "data2"
+    });
+
     res.send('hello world');
 });
-
 
 app.listen(constants.PORT, () => console.log(`app on port ${constants.PORT}`));
--- common/constants.js
+++ common/constants.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const API_BASIC_ENDPOINT_URL = "https://api.zeit.co/v2/now/";
 const LOGZIO_TOKEN = "037b265e-c010-49f6-9a99-ecfe29d5adb8"; // Moshe Basher
-
+const LOGSDNA_KEY = "33bc25c119324ac7341346450188cbc4"; // Moshe Basher
 const ZEIT_API_ROUTES = {
     LOGS: "/events",
     DEPLOYMENTS: "/deployments"
@@ -27,6 +27,7 @@ module.exports = {
     API_BASIC_ENDPOINT_URL,
     LOG_MESSAGES,
     LOGZIO_TOKEN,
+    LOGSDNA_KEY,
     ZEIT_API_ROUTES,
     ZEIT_HTTP_INSTANCE,
     PORT
