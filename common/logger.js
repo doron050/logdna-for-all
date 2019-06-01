4bb01d6d761414341425b6172cd5403b3f2b1b2b
Doron Eli <doron.eli@naturalint.com>
2019-06-02 02:04:56 +0300

    add logger

--- common/logger.js
+++ common/logger.js
@@ -1,81 +1,79 @@
-// const constants = require('./constants');
-//
-// const logzioLogger = require('logzio-nodejs').createLogger({
-//     token: process.env.LOGGER_TOKEN
-// });
+const constants = require('./constants');
+const logzioLogger = require('logzio-nodejs').createLogger({
+    token: process.env.LOGGER_TOKEN
+});
 
-//
-// const log4js = require('log4js');
-// log4js.configure({
-//     appenders: {
-//         console: {
-//             type: 'console'
-//         }
-//     },
-//     categories: {
-//         default: {
-//             appenders: ['console'],
-//             level: "debug"
-//         }
-//     }
-// });
-// const log4jslogger = log4js.getLogger();
+const log4js = require('log4js');
+log4js.configure({
+    appenders: {
+        console: {
+            type: 'console'
+        }
+    },
+    categories: {
+        default: {
+            appenders: ['console'],
+            level: "debug"
+        }
+    }
+});
+const log4jslogger = log4js.getLogger();
 
 function debugLog(msg, projId, configId, active, teamId, teamName, registrationDate, userName, userEmail) {
-    // log4jslogger.debug(msg);
-    console.log(msg);
-    // if (constants.ENVIRONMENT === "production") {
-    //     logzioLogger.log({
-    //         message: msg,
-    //         level: 'debug',
-    //         projectId: projId,
-    //         configurationId: configId,
-    //         active: active,
-    //         teamId: teamId,
-    //         registrationDate: registrationDate,
-    //         teamName: teamName,
-    //         userName: userName,
-    //         userEmail: userEmail
-    //     });
-    // }
+    log4jslogger.debug(msg);
+    // console.log(msg);
+    if (constants.ENVIRONMENT === "production") {
+        logzioLogger.log({
+            message: msg,
+            level: 'debug',
+            projectId: projId,
+            configurationId: configId,
+            active: active,
+            teamId: teamId,
+            registrationDate: registrationDate,
+            teamName: teamName,
+            userName: userName,
+            userEmail: userEmail
+        });
+    }
 }
 
 function infoLog(msg, projId, configId, active, teamId, teamName, registrationDate, userName, userEmail) {
-    // log4jslogger.info(msg);
-    console.log(msg);
-    // if (constants.ENVIRONMENT === "production") {
-    //     logzioLogger.log({
-    //         message: msg,
-    //         level: 'info',
-    //         projectId: projId,
-    //         configurationId: configId,
-    //         active: active,
-    //         teamId: teamId,
-    //         registrationDate: registrationDate,
-    //         teamName: teamName,
-    //         userName: userName,
-    //         userEmail: userEmail
-    //     });
-    // }
+    log4jslogger.info(msg);
+    // console.log(msg);
+    if (constants.ENVIRONMENT === "production") {
+        logzioLogger.log({
+            message: msg,
+            level: 'info',
+            projectId: projId,
+            configurationId: configId,
+            active: active,
+            teamId: teamId,
+            registrationDate: registrationDate,
+            teamName: teamName,
+            userName: userName,
+            userEmail: userEmail
+        });
+    }
 }
 
 function errorLog(msg, projId, configId, active, teamId, teamName, registrationDate, userName, userEmail) {
-    // log4jslogger.error(msg);
-    console.log(msg);
-    // if (constants.ENVIRONMENT === "production") {
-    //     logzioLogger.log({
-    //         message: msg,
-    //         level: 'error',
-    //         projectId: projId,
-    //         configurationId: configId,
-    //         active: active,
-    //         teamId: teamId,
-    //         registrationDate: registrationDate,
-    //         teamName: teamName,
-    //         userName: userName,
-    //         userEmail: userEmail
-    //     });
-    // }
+    log4jslogger.error(msg);
+    // console.log(msg);
+    if (constants.ENVIRONMENT === "production") {
+        logzioLogger.log({
+            message: msg,
+            level: 'error',
+            projectId: projId,
+            configurationId: configId,
+            active: active,
+            teamId: teamId,
+            registrationDate: registrationDate,
+            teamName: teamName,
+            userName: userName,
+            userEmail: userEmail
+        });
+    }
 }
 
 module.exports = {
--- heroku/app.js
+++ heroku/app.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const constants = require('../common/constants');
-const logger = require('../common/logger');
-logger.info('hiiiiiiiiiiiiiiiiiii this is my cool log');
 
 require('./clientRetriver');
 
