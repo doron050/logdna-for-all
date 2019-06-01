0b4c84577a362044864b7c17dc2917ee17878f81
Niv M <nmahnovsky@gmail.com>
2019-06-01 17:11:17 +0300

    ui

--- zeitConnector/integrationConfigServer.js
+++ zeitConnector/integrationConfigServer.js
@@ -80,7 +80,7 @@ module.exports = withUiHook(async ({payload, zeitClient}) => {
 //<Img title="Let us connect!" width="600px" height="100px" src="https://github.com/doron050/logz-for-all/blob/master/resources/images/unsplash.jpg?raw=true" />
     return htm`
     <Page>
-      <Box background-image="https://github.com/doron050/logz-for-all/blob/master/resources/images/federica.jpg?raw=true" display="flex" justifyContent="space-between">
+      <Box background-image=url("https://github.com/doron050/logz-for-all/blob/master/resources/images/federica.jpg?raw=true") display="flex" justifyContent="space-between">
       
       ${header}
       
