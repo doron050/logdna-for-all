5325d35b8b2b5578abac97df4ba333c4c81eb7df
Doron Eli <doron.eli@naturalint.com>
2019-06-02 22:31:43 +0300

    add http option handaling

--- zeit/redirectServer.js
+++ zeit/redirectServer.js
@@ -21,6 +21,17 @@ module.exports = async (req, res) => {
     console.log(`req.params = ${req.params}`);
     console.log(`req.body = ${req.body}`);
 
+    if (req.method === 'OPTIONS') {
+        const headers = {};
+        headers["Access-Control-Allow-Origin"] = "*";
+        headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+        headers["Access-Control-Allow-Credentials"] = false;
+        headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+        headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+        res.writeHead(200, headers);
+        res.end();
+    }
+
     // if (req.method === 'DELETE') {
     //     const params = req.params;
     //     const query = req.query;
