68c9728ac95ced4aade6ef17d4f8255d21fbfaf4
Niv M <nmahnovsky@gmail.com>
2019-06-09 02:16:04 +0300

    create first generic ui with const

---
+++ common/uiConstants.js
@@ -0,0 +1,22 @@
+
+const LOG_SERIVCE_NAME = "LogDNA"
+const LOGO = {
+    ON: "https://github.com/doron050/logz-for-all/blob/master/resources/images/logDNA/logDNA-Icon.png?raw=true",
+    OFF: "https://github.com/doron050/logdna-for-all/blob/master/resources/images/logDNA/%E2%80%8F%E2%80%8FlogDNA-Icon-no.png?raw=true"
+}
+const URLS ={
+    HOME_PAGE = "https://logdna.com",
+    GET_TOKEN = "https://logdna.com",
+    VIEW_LOGS = "https://app.logdna.com/logs"
+}
+const SERIVCE_AND_SUPPORT = {
+    GIT: "https://github.com/doron050/logdna-for-all",
+    EMAIL: "logs_for_all@yahoo.com"
+}
+
+module.exports = {
+    LOG_SERIVCE_NAME,
+    LOGO,
+    URLS,
+    SERIVCE_AND_SUPPORT
+};
